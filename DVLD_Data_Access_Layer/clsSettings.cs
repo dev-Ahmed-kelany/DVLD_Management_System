94159e4dbe45d27268f2f8c8eeaa27fce143335d
Ahmed <dev.ahmed.kelany@gmail.com>
2026-03-15 15:20:49 +0200

    Upload Project Initial Files

---
+++ DVLD_Data_Access_Layer/clsSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Data_Access_Layer
+{
+    public class clsSettings
+    {
+        public static string connectionString = "Server=.;Database=DVLD_Management_System;User Id=sa;Password=123456";
+    }
+}
